80626c7b65673a643681b05502f6bfcb8790345e
raluca <lacatusu.raluca@gmail.com>
2016-12-15 13:35:12 +0100

    Validation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,49 +1,54 @@
 require 'test_helper'
 
-class ProductsControllerTest < ActionController::TestCase
+class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsum',
+      description: 'Wibbles are fun!', 
+      image_url: 'lorem.jpg',
+      price: 19.95
+    }
   end
 
   test "should get index" do
-    get :index
+    get products_url
     assert_response :success
-    assert_not_nil assigns(:products)
   end
 
   test "should get new" do
-    get :new
+    get new_product_url
     assert_response :success
   end
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post products_url, params: { product: @update }
     end
 
-    assert_redirected_to product_path(assigns(:product))
+    assert_redirected_to product_url(Product.last)
   end
 
   test "should show product" do
-    get :show, id: @product
+    get product_url(@product)
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @product
+    get edit_product_url(@product)
     assert_response :success
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
-    assert_redirected_to product_path(assigns(:product))
+    patch product_url(@product), params: { product: @update }
+    assert_redirected_to product_url(@product)
   end
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
-      delete :destroy, id: @product
+      delete product_url(@product)
     end
 
-    assert_redirected_to products_path
+    assert_redirected_to products_url
   end
 end
