2290948fe9f3d0620bbde0f4c9c18997e7b51adb
raluca <lacatusu.raluca@gmail.com>
2016-12-19 14:10:05 +0100

    Added add to cart functionality

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
+  before_action :set_cart_counter
   def index
     @products = Product.order(:title)
   end
+
+  private 
+    def set_cart_counter
+      session[:counter] ||= 0
+      session[:counter] += 1
+    end
 end
