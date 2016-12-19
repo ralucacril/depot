8eda43209fa9271b66b141719074977854cb97f8
raluca <lacatusu.raluca@gmail.com>
2016-12-19 10:50:39 +0100

    Transformed erb files to haml, plus other small changes

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,13 +1,21 @@
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
     @update = {
-      title: 'Lorem Ipsum',
-      description: 'Wibbles are fun!', 
-      image_url: 'lorem.jpg',
-      price: 19.95
+      title:       'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url:   'lorem.jpg',
+      price:       19.95
     }
   end
 
@@ -29,6 +37,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(Product.last)
   end
 
+  # ...
+
   test "should show product" do
     get product_url(@product)
     assert_response :success
@@ -43,6 +53,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
   end
+
+  # ...
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
